f0729afabf5201185cc0e1cde3cea07eba95e04d
volumbe <55223434+volumbe@users.noreply.github.com>
2021-04-03 18:01:35 -0400

    Update EventsCalendar.js

--- src/components/EventsCalendar.js
+++ src/components/EventsCalendar.js
@@ -10,10 +10,8 @@ class EventsCalendar extends React.Component{
                         plugins={[ dayGridPlugin ]}
                         initialView="dayGridMonth"
                         events={[
-                            { title: 'Cryptocurrency', date: '2021-03-08' },
-                            { title: 'Adulting 101', date: '2021-02-25' },
                             { title: 'Taxes 101', date: '2021-04-01' },
-                            { title: 'Financial Hygience', date: '2021-04-15' },
+                            { title: 'Fintech Revolution', date: '2021-04-15' },
                             { title: 'Credit Cards', date: '2021-04-22' },
                           ]}
                     />
