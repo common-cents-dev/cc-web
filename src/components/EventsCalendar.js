f1297a10e108bed6a7d327b3050e0e4ada74425b
volumbe <55223434+volumbe@users.noreply.github.com>
2021-04-03 18:02:24 -0400

    Update EventsCalendar.js

--- src/components/EventsCalendar.js
+++ src/components/EventsCalendar.js
@@ -12,7 +12,7 @@ class EventsCalendar extends React.Component{
                         events={[
                             { title: 'Taxes 101', date: '2021-04-01' },
                             { title: 'Fintech Revolution', date: '2021-04-15' },
-                            { title: 'Credit Cards', date: '2021-04-22' },
+                            { title: 'Credit Card Hacks', date: '2021-04-22' },
                           ]}
                     />
           </div>
